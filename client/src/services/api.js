e3e3107765718c0d6883c86d3bfc64bd2da0bfc9
karlorz <karl.logon@gmail.com>
2024-01-07 20:18:35 +0800

    add react-app docker

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+// Get the local IP address
+const localIPAddress = window.location.hostname;
+console.log('Local IP Address:', localIPAddress);
+
+// Use the network IP address in the API URL
+const API_URL = `http://${localIPAddress}:8080`;
 
 export const savePost = async (payload) => {
     try {
